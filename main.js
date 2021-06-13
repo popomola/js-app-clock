3a7343a3a4620217f4c2eb0f97b4141ffa7dabe0
popomola <mrsimple1015@naver.com>
2021-06-13 17:08:07 +0900

    main.js

--- main.js
+++ main.js
@@ -1,21 +1,121 @@
-//시계를 화면의 가운데다가 놓을 것이다.
-//유저가 자기 이름을 입력하면 끝까지 기억하게 만든다. + 이름을 고칠 수 있게 하는 기능도 가능하면 추가
-//ToDo리스트를 시계 아래다가 만든다.
-//배경화면이 랜덤한 이미지로 랜덤한 순서로 바뀔 수 있게 한다.
-//지역의 날씨를 알려주는 기능도 넣는다.
+const divForClockAndInput = document.querySelector(".js-div");
 
-
+const H1_CLASSNAME="js-clock",
+    clockContainer = divForClockAndInput.querySelector("h1");
 //시계를 보여주는 함수
-function showClock(){}
+function showClock(){
+    clockContainer.classList.add(H1_CLASSNAME);
 
+    const date = new Date();
+    const getHours = date.getHours();
+    const getMinutes = date.getMinutes();
+    const getSeconds = date.getSeconds();
+
+    clockContainer.innerText = `${getHours}:${getMinutes}:${getSeconds}`;
+    
+}
+
+//로컬스토리지에 저장시켜주는 함수
+/*function handleSaving(key, value){
+    localStorage.setItem(key, value);
+}*/
+
+const INPUT_CLASSNAME = "js-input",
+    inputContainer = divForClockAndInput.querySelector("input");
 //유저의 이름을 받는 함수 : 이름을 인풋으로 받고 & 저장 & 화면에 보여주기 & 원하면 이름을 삭제했다가 바꿀 수 있도록
-function getUserName(){}
+function getUserName(){
+    inputContainer.classList.add(INPUT_CLASSNAME);
 
+    const btnEnter = document.createElement("button");
+    btnEnter.innerText = "Enter Name";
+    divForClockAndInput.appendChild(btnEnter);
+
+    btnEnter.addEventListener("click", function(event){
+        const h1Greeting = document.createElement("h1");
+        const name = inputContainer.value;
+        divForClockAndInput.appendChild(h1Greeting); 
+        h1Greeting.innerText = `Hello ${name}!`; 
+    });
+    //이름을 이미 입력하였다면 다시 지울 때까지 이름을 또 못 짓게 만들어야 한다.
+    //저장기능을 만들어야 하는데... 그냥 함수로 만들어서 Todo를 저장할 때도 쓸 수는 없을까?
+    localStorage.setItem("userName", inputContainer.value);
+}
+
+const divForTodoList = document.querySelector(".js-TodoList");
+
+const LI_CLASSNAME = "js-li",
+    inputContainerForTodoList = divForTodoList.querySelector("input"),
+    todoArray = [];
 //ToDo리스트를 만들 수 있는 함수. : 항목을 인풋으로 받고 & 리스트를 생성->항목을 리스트에 담고 & 저장 & 화면에 보여주기 & 삭제
-function makeToDoList(){}
+function makeToDoList(){
+
+    const btnEnter = document.createElement("button");
+    btnEnter.innerText = "Enter Todo";
+    divForTodoList.appendChild(btnEnter);
+
+    btnEnter.addEventListener("click", function(event){
+        const todoList = document.createElement("li");
+        const todo = inputContainerForTodoList.value;
+        divForTodoList.appendChild(todoList);
+        todoList.append(todo);
+        
+        const objTodo = {key:todo.toString(), context:todo};
+        todoArray.push(objTodo);
+        localStorage.setItem(objTodo.key, objTodo.context);
+
+        todoArray.forEach((item)=>{localStorage.getItem(objTodo.key)});
+
+        const btnDone = document.createElement("button");
+        btnDone.innerText = "Done";
+        todoList.appendChild(btnDone);
+        
+        btnDone.addEventListener("click", function(event){todoList.remove()});
+    });
+    
+}
 
 //랜덤한 이미지를 랜덤한 순서로 화면에 띄우는 함수 : 이미지들의 array를 만들고 & 랜덤한 순서로 화면에 보여준다.
-function rdmBackGroundImg(){}
+function rdmBackGroundImg(){
+    const images = [
+        "pink.png",
+        "Test.png",
+    ],
+        chosenImg = images[Math.floor(Math.random()*images.length)];
+    
+    document.querySelector("body").setAttribute("background", `img/${chosenImg}`);
 
+}
+
+const API_KEY = "ddcb208a36c4405aebfca2a02ef4f1a0";
 //날씨 보여주는 함수.
-function getWeatherByLocation(){}
+function getWeatherByLocation(){
+    navigator.geolocation.getCurrentPosition(
+        (position)=>{
+            const lat = position.coords.latitude; 
+            const lon = position.coords.longitude;
+            const urls = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
+
+            fetch(urls)
+            .then((response)=>response.json())
+            .then((data)=>{
+                const weather = document.querySelector("#weather span:first-child"); 
+                const city = document.querySelector("#weather span:last-child"); 
+                city.innerText = data.name; 
+                weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+            })
+        }, 
+        ()=>{
+            alert("I cannot find your location");
+        }
+    );
+}
+
+function init(){
+    setInterval(showClock, 1000);
+    getUserName();
+    makeToDoList();
+    rdmBackGroundImg();
+    getWeatherByLocation();
+}
+
+init();
